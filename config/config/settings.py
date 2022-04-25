c2bbb3bddb32b7ceb896221f0e1d380243920254
haykserobyan <89manukyan>
2022-04-25 13:39:37 +0400

    change static root

--- config/config/settings.py
+++ config/config/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = "/home/hayk/sites/www.socialreading.xyz/social-reading/config/static"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
