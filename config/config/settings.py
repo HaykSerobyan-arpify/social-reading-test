1520f6cec264a79c61fd1fe654175b3810ef1775
haykserobyan <89manukyan>
2022-04-27 02:16:11 +0400

    test1 static root

--- config/config/settings.py
+++ config/config/settings.py
@@ -123,11 +123,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 
-
-STATICFILES_DIRS = (
-    STATIC_ROOT, "/home/hayk/sites/www.socialreading.xyz/social-reading/config/static/"
-)
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
