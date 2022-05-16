1b4fa455e7d8963bee1d08916207c09f6d9f877b
haykserobyan <89manukyan>
2022-05-16 17:53:11 +0400

    change email host

--- config/config/settings.py
+++ config/config/settings.py
@@ -186,8 +186,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "hayk.serobyan.89@gmail.com"
-EMAIL_HOST_PASSWORD = "vytpzzxemxfjwayj"
-# EMAIL_HOST_PASSWORD = 'thxopkdcpafjuifm'    # socialreading.xyz
+# EMAIL_HOST_PASSWORD = "vytpzzxemxfjwayj"
+EMAIL_HOST_PASSWORD = 'thxopkdcpafjuifm'    # socialreading.xyz
 
 EMAIL_USE_TLS = True
 fail_silently = True
