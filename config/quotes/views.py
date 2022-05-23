bf5217d7e7ed864f10c335c09a9dd18d19fa56c6
haykserobyan <89manukyan>
2022-05-23 17:56:25 +0400

    change add category 12

--- config/quotes/views.py
+++ config/quotes/views.py
@@ -88,9 +88,8 @@ class QuotesViewSet(viewsets.ModelViewSet):
             new_category.users.set([user_id])
         else:
             cat = Category.objects.filter(name=category)
-            print(cat)
-            print(cat.get('users').add(user_id))
-            cat.users.add(user_id)
+            print(find_category)
+            find_category.add(user_id)
 
 
 class QuotesViewHTML(View):
