f82970eb6fb8731437d0ed55f847d44ea1f9fde5
haykserobyan <89manukyan>
2022-05-24 17:55:22 +0400

    change quote url

--- config/quotes/urls.py
+++ config/quotes/urls.py
@@ -4,7 +4,7 @@ from .views import QuotesViewSet, QuotesViewHTML, PublishQuotesViewSet
 
 router = routers.SimpleRouter()
 router.register('', QuotesViewSet)
-router.register('published/', PublishQuotesViewSet)
+router.register('published', PublishQuotesViewSet)
 
 urlpatterns = router.urls
 urlpatterns += [path('view/html/', QuotesViewHTML.as_view())]
