e5331844079a0eebfde17e333865e4f7a7c6dfdf
haykserobyan <89manukyan>
2022-06-13 00:57:54 +0400

    add .env for digitalocean

--- app/app/settings.py
+++ app/app/settings.py
@@ -30,7 +30,7 @@ DatabaseWrapper.ops_class = PatchedDatabaseOperations
 # import register.models
 
 env = environ.Env()
-environ.Env.read_env(env_file='../../env')
+environ.Env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
